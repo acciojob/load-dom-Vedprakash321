39f2585b4950ca03be3ea9873c24b340ec57bf51
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-25 01:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+// const browserInfoDiv = document.getElementById("DOM");
+// browserInfoDiv.textContent = "DOM load success";
+
+document.addEventListener("DOMContentLoaded", function() {
+            // Create a new paragraph element
+            var paragraph = document.createElement("p");
+
+            // Set the text content of the paragraph
+            paragraph.textContent = "DOM load success";
+
+            // Append the paragraph to the body of the document
+            document.body.appendChild(paragraph);
+        });
